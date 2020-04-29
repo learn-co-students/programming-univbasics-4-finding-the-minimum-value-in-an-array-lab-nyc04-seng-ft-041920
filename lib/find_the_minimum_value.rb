832c4fd163f14613e19927f7c7bd5ae893ad385f
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-29 03:26:05 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,5 +2,15 @@
 #This method should return whichever integer in the array has the lowest value.
 
 def find_min_value(array)
-  array.sort.first
+  #array.sort.first
+  count = 1
+  min = array[0]
+  
+  while count < array.length do
+   if  min - array[count] > 0
+     min = array[count]
+   end
+   count += 1
+  end
+  min
 end
